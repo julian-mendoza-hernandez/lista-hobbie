cd3864127717663f527658706649a0aca8818065
Julian Mendoza Hernández <111841380+julian-mendoza-hernandez@users.noreply.github.com>
2023-06-07 22:08:38 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div class="position-absolute top-50 start-50 translate-middle">
+      <h3>Lista de Hobbies de Jose Fernando Navarrete Valladares</h3>
+      <ol>
+        <li>Jugar lolcito</li>
+        <li>Dormir siestas</li>
+        <li>Mi niña mi niña</li>
+        <li>Ver Tiktok</li>
+      </ol>
+    </div>
+  )
+}
+
+export default App
